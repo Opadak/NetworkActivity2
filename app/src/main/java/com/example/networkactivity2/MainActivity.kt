9fe062c3eed973bca8e9aff909368f1f197c282c
Opadak <tfk1019@naver.com>
2020-11-10 23:06:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/networkactivity2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.networkactivity2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
+
+
+
+
+class PersonList(var name : String, var id : String, var age:Int,var intro: String){
+
+}
