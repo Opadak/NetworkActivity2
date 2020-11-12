a808794f91902b83c8ab4c5577cf7d32c41074c8
Opadak <tfk1019@naver.com>
2020-11-12 23:06:56 +0900

    2020-11-12 Commit

---
+++ app/src/main/java/com/example/networkactivity2/Person.kt
@@ -0,0 +1,12 @@
+package com.example.networkactivity2
+
+import java.io.Serializable
+
+class Person (
+    var id: Int? = null,
+    var name: String? = null,
+    var age: Int? = null,
+    var intro: String? = null
+
+) :Serializable
+//같은 변수명이 있는 곳을 찾아서 알려주는 것을 직렬화라고 한다.
